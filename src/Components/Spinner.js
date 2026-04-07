fc1ef65868ab2c4266ff94c028e77e19c00bca34
Reshu gupta <124056347+Reshugupta2003@users.noreply.github.com>
2026-04-07 19:10:02 +0530

    Delete src directory

--- src/Components/Spinner.js
+++
@@ -1,9 +0,0 @@
-import loading from './loading.gif';
-const Spinner = () => {
-    return (
-      <div className='text-center'>
-        <img className='my-3' src= {loading} alt="..." />
-      </div>
-    )
-}
-export default Spinner;
